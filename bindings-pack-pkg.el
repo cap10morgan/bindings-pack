2cd09fcf741422c1c6ca9df9f72a4b605f42a52b
Wes Morgan <cap10morgan@gmail.com>
2013-10-08 13:50:22 -0600

    sync version with Emacs Live

--- bindings-pack-pkg.el
+++ bindings-pack-pkg.el
@@ -1,2 +1,2 @@
-(define-package "bindings-pack" "1.0.0"
+(define-package "bindings-pack" "1.0-BETA-23"
   "Emacs Live pack that provides a carefully selected (and opinionated) set of key bindings.")
